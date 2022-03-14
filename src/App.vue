48e68ba6ccb437b94dbcffa494a9fbc3efc981de
YoussefAshraf397 <youssefashraf397@yahoo.com>
2022-03-14 15:06:10 +0200

    intialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,115 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+      <!-- Sidebar -->
+      <div class="h-0 overflow-auto flex-grow">
+        <div class="mt-4">
+          <a
+            class="flex items-center h-8 text-sm pl-8 pr-3"
+            href="#"
+            v-for="note in notes"
+            :key="note.created"
+          >
+            <span class="ml-2 leading-none">{{ new Date(note.created).toLocaleString() }}</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col flex-grow">
+      <!-- Main Content -->
+      <div class="flex flex-col flex-grow overflow-auto">
+        <editor-content :editor="editor" />
+      </div>
+      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
+        <button class="flex px-4 py-3 text-white bg-blue-500 hover:bg-blue-400" @click="saveNote">Save Note</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Editor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EditorContent
+  },
+  data() {
+    return {
+      editor: null,
+      database: null,
+      notes: []
+    }
+  },
+  async created() {
+    this.database = await this.getDatabase();
+    let notes = await this.getNotes();
+    this.notes = notes.reverse();
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: '',
+      extensions: [
+        StarterKit
+      ],
+      editorProps: {
+        attributes: {
+          class: "prose my-6 mx-auto focus:outline-none"
+        }
+      }
+    });
+  },
+  beforeUnmount() {
+    this.editor.destroy();
+  },
+  methods: {
+    async getDatabase() {
+      return new Promise((resolve, reject) => {
+        let db = window.indexedDB.open("notes", 2);
+        db.onerror = e => {
+          reject(e,'Error opening the database.');
+        };
+        db.onsuccess = e => {
+          console.log('db.onsuccess', e);
+          resolve(e.target.result);
+        };
+        db.onupgradeneeded = e => {
+          console.log('db.onupgradeneeded', e);
+          e.target.result.deleteObjectStore("notes");
+          e.target.result.createObjectStore("notes", { keyPath: "created" });
+        };
+      });
+    },
+    async saveNote() {
+      return new Promise((resolve, reject) => {
+        let transaction = this.database.transaction('notes', 'readwrite');
+        console.log(reject)
+        transaction.oncomplete = e => {
+          console.log(e)    
+          resolve();
+        }
+        let now = new Date();
+        let note = {
+          content: this.editor.getHTML(),
+          created: now.getTime()
+        };
+        this.notes.unshift(note)
+        transaction.objectStore('notes').add(note);
+      });
+    },
+    async getNotes() {
+      return new Promise((resolve, reject) => {
+          console.log(reject)
+        this.database.transaction('notes')
+          .objectStore('notes')
+          .getAll()
+          .onsuccess = e => {
+            console.log('getNotes()', e.target.result);
+            resolve(e.target.result);
+          };
+      });
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
