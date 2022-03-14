9a2c97d2a8c43d989f2e26144fc44fcb8ac41c26
YoussefAshraf397 <youssefashraf397@yahoo.com>
2022-03-14 17:20:05 +0200

    stable version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pwa: {
+    // workboxPluginMode: "InjectManifest",
+        workboxOptions: {
+            skipWaiting: true,
+            // swSrc: "./src/service-worker.js"
+        }
+    }
+}
